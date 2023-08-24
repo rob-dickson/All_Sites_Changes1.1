eb3f690a82723a4fc195e8b14b7f1410cdf6d678
rob-dickson <dickson.rob88@gmail.com>
2023-08-24 12:28:28 -0600

    Commit

---
+++ Templates/C2.vb
@@ -0,0 +1,47 @@
+
+
+ifs(len(HeadshotUI)>0, 
+"<font face='FTPolar-Semibold' fontsize='10pt'>" 
+& UPPER(FullNameUI) 
+& "<BR>" 
+& "</font>" 
+& "<font face='FTPolar-Semibold' fontsize='8pt'>" 
+& TitleUI 
+& ifs(DepartmentUI="","",", " 
+& "<BR>" & DepartmentUI) 
+& "</font>", "")
+
+
+ifs(len(HeadshotUI)>0, 
+"<font face='FTPolar-Semibold' fontsize='10pt'>" 
+& UPPER(FullNameUI) 
+& "<BR>" 
+& "</font>" 
+& "<font face='FTPolar-Semibold' fontsize='8pt'>" 
+& ifs(len(TitleUI)>30,
+"<font fontsize='8pt'>"
+& TitleUI
+& "</font>", TitleUI)
+& ifs(DepartmentUI="","",", " 
+& "<BR>" & DepartmentUI) 
+& "</font>", "")
+
+
+
+
+
+John Jameson
+Cerified Reverse Mortgage Specialist
+Louisville KY Resident
+
+10509 Vista Sorreto Pkwy Ste 400
+San Diego, CA 92121
+
+502.544.2119
+jjameson@c2financial.com
+reverselouisville.com
+
+455553
+MC751512
+
+
