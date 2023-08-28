03dfe8da28260d3300faa64b10ba1a276296f6a8
rob-dickson <dickson.rob88@gmail.com>
2023-08-28 14:11:05 -0600

    commit

--- Templates/C2.vb
+++ Templates/C2.vb
@@ -1,31 +1,41 @@
 
 
-ifs(len(HeadshotUI)>0, 
-"<font face='FTPolar-Semibold' fontsize='10pt'>" 
+ifs(len(HeadshotUI)>0, "<font face='FTPolar-Semibold' fontsize='10pt'>" 
 & UPPER(FullNameUI) 
-& "<BR>" 
-& "</font>" 
-& "<font face='FTPolar-Semibold' fontsize='8pt'>" 
-& TitleUI 
-& ifs(DepartmentUI="","",", " 
-& "<BR>" & DepartmentUI) 
+& "<BR>" & "</font>" & "<font face='FTPolar-Semibold' fontsize='8pt'>" 
+& TitleUI & ifs(DepartmentUI="","",", " & "<BR>" & DepartmentUI) 
 & "</font>", "")
 
+---------------------------------------------------------------------------------------------------------------------------------------------
 
-ifs(len(HeadshotUI)>0, 
-"<font face='FTPolar-Semibold' fontsize='10pt'>" 
+ifs(len(HeadshotUI)>0, "<font face='FTPolar-Semibold' fontsize='10pt'>" 
 & UPPER(FullNameUI) 
-& "<BR>" 
-& "</font>" 
-& "<font face='FTPolar-Semibold' fontsize='8pt'>" 
-& ifs(len(TitleUI)>30,
-"<font fontsize='8pt'>"
-& TitleUI
-& "</font>", TitleUI)
-& ifs(DepartmentUI="","",", " 
-& "<BR>" & DepartmentUI) 
+& "<BR>" & "</font>" & "<font face='FTPolar-Semibold' fontsize='6pt'>" 
+& TitleUI & "</font>" & "<font face='FTPolar-Semibold' fontsize='8pt'>" & ifs(DepartmentUI="","",", " & "<BR>" & DepartmentUI) 
 & "</font>", "")
 
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+ifs(len(HeadshotUI)>0, "<font face='FTPolar-Semibold' fontsize='10pt'>" & UPPER(FullNameUI) & "<BR>" & "</font>" 
+
+& "<font face='FTPolar-Semibold' fontsize='8pt'>"
+
+& ifs(len(TitleUI)>1.5, "<font face='FTPolar-Semibold' fontsize='6.5pt'>" & TitleUI & "</font>", "")
+
+& ifs(DepartmentUI="","",", " & "<BR>" & DepartmentUI) & "</font>", "")
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+ifs(len(HeadshotUI)>0, "<font face='FTPolar-Semibold' fontsize='10pt'>" & UPPER(FullNameUI) & "<BR>" & "</font>" 
+
+& "<font face='FTPolar-Semibold' fontsize='8pt'>"
+
+& ifs(len(TitleUI)>25, "<font face='FTPolar-Semibold' fontsize='6pt'>" & TitleUI & "<BR>" & DepartmentUI & "</font>", 
+"<font face='FTPolar-Semibold' fontsize='8pt'>" & TitleUI & "<BR>" & DepartmentUI & "</font>")
+
+& ifs(DepartmentUI="","",", " & "<BR>" & DepartmentUI) & "</font>", "")
+
+---------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
