1dda09f7b6c50ab8d409b84b3447843ffe68c62f
rob-dickson <dickson.rob88@gmail.com>
2023-08-31 08:04:47 -0600

    Commit

--- Templates/C2.vb
+++ Templates/C2.vb
@@ -1,5 +1,51 @@
 
 
+ifs(len(HeadshotUI)>0, 
+"<font face='FTPolar-Semibold' fontsize='10pt'>" 
+& UPPER(FullNameUI) 
+& "<BR>" 
+& "</font>" 
+& "<font face='FTPolar-Semibold' fontsize='8pt'>" 
+& TitleUI 
+& ifs(DepartmentUI="","",", " 
+& "<BR>" & DepartmentUI) 
+& "</font>", "")
+
+
+ifs(len(HeadshotUI)>0, 
+"<font face='FTPolar-Semibold' fontsize='10pt'>" 
+& UPPER(FullNameUI) 
+& "<BR>" 
+& "</font>" 
+& "<font face='FTPolar-Semibold' fontsize='8pt'>" 
+& "<font fontsize='8pt'>"
+& TitleUI
+& "</font>"
+& ifs(DepartmentUI="","",", " 
+& "<BR>" & DepartmentUI) 
+& "</font>", "")
+
+
+
+
+
+John Jameson
+Cerified Reverse Mortgage Specialist
+Louisville KY Resident
+
+10509 Vista Sorreto Pkwy Ste 400
+San Diego, CA 92121
+
+502.544.2119
+jjameson@c2financial.com
+reverselouisville.com
+
+455553
+MC751512
+
+
+
+
 ifs(len(HeadshotUI)>0, "<font face='FTPolar-Semibold' fontsize='10pt'>" 
 & UPPER(FullNameUI) 
 & "<BR>" & "</font>" & "<font face='FTPolar-Semibold' fontsize='8pt'>" 
@@ -36,22 +82,4 @@ ifs(len(HeadshotUI)>0, "<font face='FTPolar-Semibold' fontsize='10pt'>" & UPPER(
 & ifs(DepartmentUI="","",", " & "<BR>" & DepartmentUI) & "</font>", "")
 
 ---------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-John Jameson
-Cerified Reverse Mortgage Specialist
-Louisville KY Resident
-
-10509 Vista Sorreto Pkwy Ste 400
-San Diego, CA 92121
-
-502.544.2119
-jjameson@c2financial.com
-reverselouisville.com
-
-455553
-MC751512
-
 
