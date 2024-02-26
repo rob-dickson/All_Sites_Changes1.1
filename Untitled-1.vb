b07794a267c37c8593b1cf80c662645324b901a8
rob-dickson <dickson.rob88@gmail.com>
2024-02-26 16:52:01 -0700

    Commit

---
+++ Untitled-1.vb
@@ -0,0 +1,26 @@
+CardFDesign (graphic)
+ifs(MarketingList="Athletics","athletics-inside.pdf",
+ifs(MarketingList="BYU","byu-inside.pdf",
+ifs(MarketingList="BYUHawaii","byuh-inside.pdf",
+ifs(MarketingList="BYUIdaho","byui-inside.pdf",
+ifs(MarketingList="Pathways","pathways-inside.pdf",
+ifs(MarketingList="Ensign","ensign-inside.pdf",
+""))))))
+
+EnvelopeDesign (graphic)
+ifs(MarketingList="Athletics","athletics-env.pdf",
+ifs(MarketingList="BYU","byu-env.pdf",
+ifs(MarketingList="BYUHawaii","byuh-env.pdf",
+ifs(MarketingList="BYUIdaho","byui-env.pdf",
+ifs(MarketingList="Pathways","pathways-env.pdf",
+ifs(MarketingList="Ensign","ensign-env.pdf",
+""))))))
+
+BackDESIGN (graphic)
+ifs(MarketingList="Athletics","athletics-outside.pdf",
+ifs(MarketingList="BYU","byu-outside.pdf",
+ifs(MarketingList="BYUHawaii","byuh-outside.pdf",
+ifs(MarketingList="BYUIdaho","byui-outside.pdf",
+ifs(MarketingList="Pathways","pathways-outside.pdf",
+ifs(MarketingList="Ensign","ensign-outside.pdf",
+""))))))
