8cdba645f49762a64a3936338bf1bd26854af2d9
rob-dickson <dickson.rob88@gmail.com>
2024-08-02 17:00:37 -0600

    Commit

--- PyTest/pull.py
+++ PyTest/pull.py
@@ -1,33 +1,30 @@
-import requests
-import os
-import csv
+# import requests
+# import os
+# import csv
 
-# Directory to save the images
-directory = 'images'
+# # Directory to save the images
+# directory = 'images'
 
-# Create the directory if it doesn't exist
-if not os.path.exists(directory):
-    os.makedirs(directory)
+# # Create the directory if it doesn't exist
+# if not os.path.exists(directory):
+#     os.makedirs(directory)
 
-# CSV file containing the URLs
-csv_file = 'url.csv'
-#
+# # CSV file containing the URLs
+# csv_file = 'url.csv'
+# #
 
-# Read the CSV file and download the images
-with open(csv_file, 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        url = row[0]
-        filename = url.split('/')[-1]
-        filepath = os.path.join(directory, filename)
-        response = requests.get(url)
-        if response.status_code == 200:
-            with open(filepath, 'wb') as image_file:
-                image_file.write(response.content)
-            print(f'Downloaded {url} to {directory}')
-        else:
-            print(
-                f'Failed to download {url}. Status code: {response.status_code}')
-
-
-
+# # Read the CSV file and download the images
+# with open(csv_file, 'r') as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#         url = row[0]
+#         filename = url.split('/')[-1]
+#         filepath = os.path.join(directory, filename)
+#         response = requests.get(url)
+#         if response.status_code == 200:
+#             with open(filepath, 'wb') as image_file:
+#                 image_file.write(response.content)
+#             print(f'Downloaded {url} to {directory}')
+#         else:
+#             print(
+#                 f'Failed to download {url}. Status code: {response.status_code}')
