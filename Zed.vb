a5bb1b997f7c0b8da16f937b49f4715bcf382d13
rob-dickson <dickson.rob88@gmail.com>
2025-01-03 16:37:26 -0700

    Commit

---
+++ Zed.vb
@@ -0,0 +1,34 @@
+
+
+"Has successfully completed the" & ifs(CertificationDD = "3", " basic training program in", "") & "<BR>" &
+ifs(CertificationDD = "1", "<font face='HelveticaNeueLTStd-Bd' fontsize='14pt'>FORKLIFT OPERATOR SAFETY TRAINING PROGRAM</font><BR>" & ClassNameUI,
+ifs(CertificationDD = "2", "<font face='HelveticaNeueLTStd-Bd' fontsize='14pt'>MEWP OPERATOR SAFETY TRAINING PROGRAM<BR>Group A and B MEWP</font><BR>Type 1 and 3",
+ifs(CertificationDD = "3", "<font face='HelveticaNeueLTStd-Bd' fontsize='14pt'>Rigging, Hoisting, and Signal Person</font><BR> and has demonstrated knowledge and understanding per OSHA 29 CFR requirements",
+ifs(CertificationDD = "4", "<font face='HelveticaNeueLTStd-Bd' fontsize='14pt'>FALL PROTECTION COMPETENT PERSON</font><BR>29 CFR 1926.500 subpart M Fall Protection", "")
+                )
+            )
+        )
+
+
+
+
+
+"text" &
+    ifs(CertificationDD = "3", " text", "") & "<BR>" &
+    ifs(CertificationDD = "1", "text" & ClassNameUI,
+        ifs(CertificationDD = "2", "text",
+            ifs(CertificationDD = "3", "text",
+                ifs(CertificationDD = "4", "text", "")
+                )
+            )
+        )
+
+
+
+
+"Has successfully completed the" & ifs(CertificationDD="3"," basic training program in","") & "<BR>" & 
+ifs(CertificationDD="1","<font face='HelveticaNeueLTStd-Bd' fontsize='14pt'>FORKLIFT OPERATOR SAFETY TRAINING PROGRAM</font><BR>" & ClassNameUI,
+ifs(CertificationDD="2","<font face='HelveticaNeueLTStd-Bd' fontsize='14pt'>MEWP OPERATOR SAFETY TRAINING PROGRAM<BR>Group A and B MEWP</font><BR>Type 1 and 3",
+ifs(CertificationDD="3","<font face='HelveticaNeueLTStd-Bd' fontsize='14pt'>Rigging, Hoisting, and Signal Person</font><BR> and has demonstrated knowledge and 
+understanding per OSHA 29 CFR requirements",
+ifs(CertificationDD="4","<font face='HelveticaNeueLTStd-Bd' fontsize='14pt'>FALL PROTECTION COMPETENT PERSON</font><BR>29 CFR 1926.500 subpart M Fall Protection",""))))
